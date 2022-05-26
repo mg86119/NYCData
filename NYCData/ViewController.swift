73018c07cff3d03d7eb922d2a77ec599b9cd9355
Murali Krishna Gajula <muralikrishna.gajula3@gmail.com>
2022-05-26 18:53:42 -0400

    Initial Commit

---
+++ NYCData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NYCData
+//
+//  Created by Murali krishna Gajula on 5/26/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
